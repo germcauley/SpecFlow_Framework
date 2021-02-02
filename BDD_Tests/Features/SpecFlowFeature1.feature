70e438769947c2c7c98d11bcab6b878a2ba76c17
germcauley <geraldmcauley@gmail.com>
2021-02-02 20:36:46 +0000

    working test

---
+++ BDD_Tests/Features/SpecFlowFeature1.feature
@@ -0,0 +1,7 @@
+﻿Feature: SpecFlowFeature1
+	Simple test to open a page
+
+@mytag
+Scenario: Add two numbers
+	Given I Open Google
+	Then The page opens and I close test
---
+++ BDD_Tests/Steps/SpecFlowFeature1Steps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace BDD_Tests.Steps
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        IWebDriver webdriver;
+
+        [Given(@"I Open Google")]
+        public void GivenIOpenGoogle()
+        {
+            var outputDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var relPath = @"..\..\Drivers";
+            var fullPath = Path.GetFullPath(Path.Combine(outputDir, relPath));
+            webdriver = new ChromeDriver(fullPath);
+            webdriver.Navigate().GoToUrl("https://www.google.ie");
+        }
+        
+        [Then(@"The page opens and I close test")]
+        public void ThenThePageOpens()
+        {
+            Assert.That(webdriver.Title == "Google");
+            webdriver.Quit();
+        }
+    }
+}
